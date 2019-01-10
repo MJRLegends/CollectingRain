4f399424edd3380dd343d5fd1ce2d775004e84b2
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-10 00:28:45 +0000

    Released v1.0.3, Set minimum version of MJRLegendsLib requirement to v1.1.8

--- src/main/java/com/mjr/collectingrain/CollectingRain.java
+++ src/main/java/com/mjr/collectingrain/CollectingRain.java
@@ -17,10 +17,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = CollectingRain.MODID, version = CollectingRain.VERSION, dependencies = "required-after:forge@[14.23.1.2555,); required-after:mjrlegendslib@[1.12.2-1.1.7,);", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
+@Mod(modid = CollectingRain.MODID, version = CollectingRain.VERSION, dependencies = "required-after:forge@[14.23.1.2555,); required-after:mjrlegendslib@[1.12.2-1.1.8,);", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
 public class CollectingRain {
 	public static final String MODID = "collectingrain";
-	public static final String VERSION = "1.12.2-1.0.2";
+	public static final String VERSION = "1.12.2-1.0.3";
 
 	@SidedProxy(clientSide = "com.mjr.collectingrain.proxy.ClientProxy", serverSide = "com.mjr.collectingrain.proxy.CommonProxy")
 	public static CommonProxy proxy;
