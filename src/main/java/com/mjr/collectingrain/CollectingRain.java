30f666fc3247b34b16bed70551a31756bcf03be6
MJRLegends <MJRLegends@users.noreply.github.com>
2019-01-20 22:15:55 +0000

    Released v1.0.3

--- src/main/java/com/mjr/collectingrain/CollectingRain.java
+++ src/main/java/com/mjr/collectingrain/CollectingRain.java
@@ -26,7 +26,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = CollectingRain.MODID, version = CollectingRain.VERSION, dependencies = "required-after:forge@[13.20.1.2513,); required-after:mjrlegendslib@[1.11.2-1.1.5,);", certificateFingerprint = "b02331787272ec3515ebe63ecdeea0d746653468")
 public class CollectingRain {
 	public static final String MODID = "collectingrain";
-	public static final String VERSION = "1.11.2-1.0.2";
+	public static final String VERSION = "1.11.2-1.0.3";
 
 	@SidedProxy(clientSide = "com.mjr.collectingrain.proxy.ClientProxy", serverSide = "com.mjr.collectingrain.proxy.CommonProxy")
 	public static CommonProxy proxy;
